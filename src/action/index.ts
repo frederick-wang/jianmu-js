b95998a92e52fe1a1ef2d573c24faf80fa3fd4df
Zhaoji Wang <hwoam@outlook.com>
2023-02-06 05:25:50 +0800

    feat(action): 增加对 ElMessage 的支持

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,9 +1,7 @@
+import { ElMessage } from 'element-plus'
 import { Socket } from 'socket.io-client'
 
-let socket: Socket | null = null
-
-const initActionSocket = (s: Socket) => {
-  socket = s
+const initActionSocket = (socket: Socket) => {
   socket.on(
     'Action:show-open-dialog',
     ({ args: [options] }: { args: [Electron.OpenDialogOptions] }, callback) => {
@@ -68,6 +66,158 @@ const initActionSocket = (s: Socket) => {
   socket.on('Action:beep', (_: { args: [] }, callback) => {
     window.api.beep().then(callback)
   })
+
+  initElMessageActions(socket)
+}
+
+const initElMessageActions = (socket: Socket) => {
+  const TITLEBAR_HEIGHT = 30
+  const DEFAULT_OFFSET = TITLEBAR_HEIGHT + 20
+
+  socket.on(
+    'Action:el-message',
+    ({ args: [params] }: { args: [Record<any, any> | string] }, callback) => {
+      // params is string
+      if (typeof params === 'string') {
+        ElMessage({
+          message: params,
+          offset: DEFAULT_OFFSET,
+          onClose() {
+            callback()
+          }
+        })
+        return
+      }
+      ElMessage({
+        ...params,
+        offset: params.offset
+          ? params.offset + TITLEBAR_HEIGHT
+          : DEFAULT_OFFSET,
+        onClose() {
+          callback()
+        }
+      })
+    }
+  )
+
+  socket.on(
+    'Action:el-message.success',
+    ({ args: [params] }: { args: [Record<any, any> | string] }, callback) => {
+      // params is string
+      if (typeof params === 'string') {
+        ElMessage.success({
+          message: params,
+          offset: DEFAULT_OFFSET,
+          onClose() {
+            callback()
+          }
+        })
+        return
+      }
+      ElMessage.success({
+        ...params,
+        offset: params.offset
+          ? params.offset + TITLEBAR_HEIGHT
+          : DEFAULT_OFFSET,
+        onClose() {
+          callback()
+        }
+      })
+    }
+  )
+
+  socket.on(
+    'Action:el-message.warning',
+    ({ args: [params] }: { args: [Record<any, any> | string] }, callback) => {
+      // params is string
+      if (typeof params === 'string') {
+        ElMessage.warning({
+          message: params,
+          offset: DEFAULT_OFFSET,
+          onClose() {
+            callback()
+          }
+        })
+        return
+      }
+      ElMessage.warning({
+        ...params,
+        offset: params.offset
+          ? params.offset + TITLEBAR_HEIGHT
+          : DEFAULT_OFFSET,
+        onClose() {
+          callback()
+        }
+      })
+    }
+  )
+
+  socket.on(
+    'Action:el-message.info',
+    ({ args: [params] }: { args: [Record<any, any> | string] }, callback) => {
+      // params is string
+      if (typeof params === 'string') {
+        ElMessage.info({
+          message: params,
+          offset: DEFAULT_OFFSET,
+          onClose() {
+            callback()
+          }
+        })
+        return
+      }
+      ElMessage.info({
+        ...params,
+        offset: params.offset
+          ? params.offset + TITLEBAR_HEIGHT
+          : DEFAULT_OFFSET,
+        onClose() {
+          callback()
+        }
+      })
+    }
+  )
+
+  socket.on(
+    'Action:el-message.error',
+    ({ args: [params] }: { args: [Record<any, any> | string] }, callback) => {
+      // params is string
+      if (typeof params === 'string') {
+        ElMessage.error({
+          message: params,
+          offset: DEFAULT_OFFSET,
+          onClose() {
+            callback()
+          }
+        })
+        return
+      }
+      ElMessage.error({
+        ...params,
+        offset: params.offset
+          ? params.offset + TITLEBAR_HEIGHT
+          : DEFAULT_OFFSET,
+        onClose() {
+          callback()
+        }
+      })
+    }
+  )
+
+  socket.on(
+    'Action:el-message.close-all',
+    ({
+      args: [type_param]
+    }: {
+      args: ['success' | 'warning' | 'info' | 'error' | null]
+    }) => {
+      if (!type_param) {
+        ElMessage.closeAll()
+        return
+      }
+      ElMessage.closeAll(type_param)
+    }
+  )
 }
 
 export { initActionSocket }
